cbb397f99acd34cbf3dd321eda1b014944e8983b
sakanatsuri <71263670+irebasunglass@users.noreply.github.com>
2020-09-21 11:33:30 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <link rel="stylesheet" href="style.css"></link>
+  <link rel="stylesheet" href="gimmick/gimmick.css"></link>
+  <link rel="stylesheet" href="gimmick/blackbass/blackbass.css"></link>
+
+<title>魚釣り</title>
+<meta charset="utf-8">
+
+
+</head>
+<body>
+<header class = "header">
+
+  <img src="sakana.png" width="auto" height="100px"></img>
+  <div class = 'shokai'>こんにちは、魚釣りを楽しんでいる、とある県民です。<div/>
+  </header>
+
+  <span id ="today"></span>
+
+<a href="ep/ep1.php"> <div class = 'picture_position'><img src="picture/UBYI2966.JPG" width="auto" height="200px"></img></div></a>
+
+<script src="script/jcanvas.min.js"></script>
+<script src="script/tenki.js"></script>
+<script src="script/day.js"></script>
+<script src="script/anime.min.js"></script>
+<script src="gimmick/gimmick.js"></script>
+<script src="gimmick/blackbass/blackbass.js"></script>
+
+
+
+
+</body>
+
+</html>
